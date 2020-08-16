96b030c0735e5809b2cce90b3c9e2825ff04b0d6
kao.ring <kao.ring@yahoo.com>
2020-08-16 19:56:14 -0400

    kc

--- public/js/members.js
+++ public/js/members.js
@@ -35,4 +35,17 @@ $(document).ready(function () {
       }
     });
   });
+
+  $(".delete-post").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/posts/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted posts", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
